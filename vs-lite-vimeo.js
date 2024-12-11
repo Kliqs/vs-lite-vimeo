73e9a7ab9b8267ffcc06553479dca406b7259b1d
Tuan <minhtuan2086@gmail.com>
2024-12-11 17:24:05 +0700

    init source

--- vs-lite-vimeo.js
+++ vs-lite-vimeo.js
@@ -146,7 +146,11 @@ class LiteVimeo extends (globalThis.HTMLElement ?? class {}) {
     // Once the user clicks, add the real iframe and drop our play button
     // TODO: In the future we could be like amp-youtube and silently swap in the iframe during idle time
     //   We'd want to only do this for in-viewport or near-viewport ones: https://github.com/ampproject/amphtml/pull/5003
-    this.addEventListener('click', this.addIframe);
+    // this.addEventListener('click', this.addIframe);
+    setTimeout(() => {
+      this.addIframe
+    }, 300);
+
   }
 
   addIframe() {
@@ -154,13 +158,12 @@ class LiteVimeo extends (globalThis.HTMLElement ?? class {}) {
     this.classList.add('ltv-activated');
 
     this.videoLoop = this.getAttribute('videoloop');
-
-
     const iframeEl = document.createElement('iframe');
+
     iframeEl.width = 640;
     iframeEl.height = 360;
     // No encoding necessary as [title] is safe. https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#:~:text=Safe%20HTML%20Attributes%20include
-    iframeEl.title = this.playLabel;
+    iframeEl.title = '';
     iframeEl.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
 
 
@@ -190,36 +193,4 @@ function addPrefetch(kind, url, as) {
   }
   linkElem.crossorigin = true;
   document.head.append(linkElem);
-}
-
-/**
- * Get the thumbnail dimensions to use for a given player size.
- *
- * @param {Object} options
- * @param {number} options.width The width of the player
- * @param {number} options.height The height of the player
- * @return {Object} The width and height
- */
-function getThumbnailDimensions({ width, height }) {
-  let roundedWidth = width;
-  let roundedHeight = height;
-
-  // If the original width is a multiple of 320 then we should
-  // not round up. This is to keep the native image dimensions
-  // so that they match up with the actual frames from the video.
-  //
-  // For example 640x360, 960x540, 1280x720, 1920x1080
-  //
-  // Round up to nearest 100 px to improve cacheability at the
-  // CDN. For example, any width between 601 pixels and 699
-  // pixels will render the thumbnail at 700 pixels width.
-  if (roundedWidth % 320 !== 0) {
-    roundedWidth = Math.ceil(width / 100) * 100;
-    roundedHeight = Math.round((roundedWidth / width) * height);
-  }
-
-  return {
-    width: roundedWidth,
-    height: roundedHeight
-  };
 }
